07a0b6caca619e3a0a9725d4e36d9c1968daa23d
Toby Griffin <toby.griffin@me.com>
2016-05-24 22:17:24 +0000

    Work on Location model. Might change up table structure a lot but API should remain.

--- ossm-core/Sources/ossm-core/Models/Location.swift
+++ ossm-core/Sources/ossm-core/Models/Location.swift
@@ -15,13 +15,13 @@ CREATE INDEX "locations_key" ON "locations" ("l1", "l2", "l3", "l4");
 
 public struct Location {
 
-  let l1: Int
+  let l1: Int?
   let l2: Int?
   let l3: Int?
   let l4: Int?
   public let name: String
 
-  public init(_ l1: Int, _ l2: Int?, _ l3: Int?, _ l4: Int?, name: String) {
+  public init(_ l1: Int?, _ l2: Int?, _ l3: Int?, _ l4: Int?, name: String) {
     self.l1 = l1
     self.l2 = l2
     self.l3 = l3
@@ -29,11 +29,12 @@ public struct Location {
     self.name = name
   }
 
-  public static func keysAreValid(_ l1: Int, _ l2: Int?, _ l3: Int?, _ l4: Int?) -> Bool {
-    if (l2 == nil && l3 == nil && l4 == nil) { return true }
-    if (l2 != nil && l3 == nil && l4 == nil) { return true }
-    if (l2 != nil && l3 != nil && l4 == nil) { return true }
-    if (l2 != nil && l3 != nil && l4 != nil) { return true }
+  public static func keysAreValid(_ l1: Int?, _ l2: Int?, _ l3: Int?, _ l4: Int?) -> Bool {
+    if (l1 == nil && l2 == nil && l3 == nil && l4 == nil) { return true }
+    if (l1 != nil && l2 == nil && l3 == nil && l4 == nil) { return true }
+    if (l1 != nil && l2 != nil && l3 == nil && l4 == nil) { return true }
+    if (l1 != nil && l2 != nil && l3 != nil && l4 == nil) { return true }
+    if (l1 != nil && l2 != nil && l3 != nil && l4 != nil) { return true }
     return false
   }
 
@@ -44,20 +45,17 @@ extension Location {
 
   public init?(row: Row) {
     do {
-      guard let
-        l1: Int = try row.value("l1"),
-        name: String = try row.value("name")
-      else {
+      guard let name: String = try row.value("name") else {
         return nil
       }
-      self.init(l1, try? row.value("l2"), try? row.value("l3"), try? row.value("l4"), name: name)
+      self.init(try? row.value("l1"), try? row.value("l2"), try? row.value("l3"), try? row.value("l4"), name: name)
     } catch let error {
       log("Init error: \(error)", level: .Error)
       return nil
     }
   }
 
-  public static func get(_ l1: Int, _ l2: Int?, _ l3: Int?, _ l4: Int?) -> Location? {
+  public static func get(_ l1: Int?, _ l2: Int? = nil, _ l3: Int? = nil, _ l4: Int? = nil) -> Location? {
     // Given level ids, get the row
     if !Location.keysAreValid(l1, l2, l3, l4) {
       return nil
@@ -67,14 +65,16 @@ extension Location {
     do {
       let result: PostgreSQL.Result = try {
         switch (l1, l2, l3, l4) {
-        case (let l1, nil, nil, nil):
-          return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 IS NULL AND l3 IS NULL AND l4 IS NULL", parameters: l1)
-        case (let l1, let l2, nil, nil):
-          return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 = %@ AND l3 IS NULL AND l4 IS NULL", parameters: l1, l2)
-        case (let l1, let l2, let l3, nil):
-          return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 = %@ AND l3 = %@ AND l4 IS NULL", parameters: l1, l2, l3)
-        case (let l1, let l2, let l3, let l4):
-          return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 = %@ AND l3 = %@ AND l4 = %@", parameters: l1, l2, l3, l4)
+          case (nil, nil, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 IS NULL AND l2 IS NULL AND l3 IS NULL AND l4 IS NULL")
+          case (let l1, nil, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ AND l2 IS NULL AND l3 IS NULL AND l4 IS NULL", parameters: l1)
+          case (let l1, let l2, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ AND l2 = %@ AND l3 IS NULL AND l4 IS NULL", parameters: l1, l2)
+          case (let l1, let l2, let l3, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ AND l2 = %@ AND l3 = %@ AND l4 IS NULL", parameters: l1, l2, l3)
+          case (let l1, let l2, let l3, let l4):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ AND l2 = %@ AND l3 = %@ AND l4 = %@", parameters: l1, l2, l3, l4)
         }
       }()
       guard let row = result.first else {
@@ -87,6 +87,53 @@ extension Location {
       log("SQL error: \(db().mostRecentError)", level: .Error)
     }
     return nil
+  }
+  
+  /**
+  Return this Location's parent, or nil if this is a top-level location.
+  */
+  public func getParent() -> Location? {
+    switch (l1, l2, l3, l4) {
+      case (nil, nil, nil, nil):
+        return nil
+      case (_, nil, nil, nil):
+        return Location.get(nil, nil, nil, nil)
+      case (let l1, _, nil, nil):
+        return Location.get(l1, nil, nil, nil)
+      case (let l1, let l2, _, nil):
+        return Location.get(l1, l2, nil, nil)
+      case (let l1, let l2, let l3, _):
+        return Location.get(l1, l2, l3, nil)
+      }
+  }
+  
+  /**
+  Return an array of child Locations.
+  */
+  public func getChildren() -> [Location] {
+    if l1 != nil && l2 != nil && l3 != nil && l4 != nil { return [] }
+    do {
+      let result: PostgreSQL.Result? = try {
+        switch (l1, l2, l3, l4) {
+          case (nil, nil, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 IS NOT NULL and l2 IS NULL AND l3 IS NULL AND l4 IS NULL")
+          case (let l1, nil, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 IS NOT NULL AND l3 IS NULL AND l4 IS NULL", parameters: l1)
+          case (let l1, let l2, nil, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 = %@ AND l3 IS NOT NULL AND l4 IS NULL", parameters: l1, l2)
+          case (let l1, let l2, let l3, nil):
+            return try db().execute("SELECT * FROM locations WHERE l1 = %@ and l2 = %@ AND l3 = %@ AND l4 IS NOT NULL", parameters: l1, l2, l3)
+          default:
+            return nil
+        }
+      }()
+      if let result = result {
+        return result.flatMap({ Location(row: $0) })
+      }
+    } catch {
+      log("SQL error: \(db().mostRecentError)", level: .Error)
+    }
+    return []
   }
 
 }
--- ossm-core/Tests/ossm-core/Models/LocationTests.swift
+++ ossm-core/Tests/ossm-core/Models/LocationTests.swift
@@ -9,27 +9,40 @@ class LocationTests: XCTestCase {
     try! configureDatabase(host: "127.0.0.1", port: 5432, username: "ossm", password: "abracadabra", databaseName: "ossm-test")
     try! db().execute("DROP TABLE IF EXISTS locations")
     try! db().execute("CREATE TABLE locations (l1 smallint, l2 smallint, l3 smallint, l4 smallint, name varchar(40) not null)")
-    try! db().execute("CREATE INDEX locations_key ON locations (l1, l2, l3, l4)")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1, NULL, NULL, NULL, 'Oceania')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1, NULL, NULL, 'Australia')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1,    1, NULL, 'New South Wales')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1,    1,    1, 'Sydney')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1,    1,    2, 'Blue Mountains')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1,    2, NULL, 'Victoria')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    1,    2,    1, 'Melbourne')")
-    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1,    2, NULL, NULL, 'New Zealand')")
+    try! db().execute("CREATE UNIQUE INDEX locations_key ON locations (l1, l2, l3, l4)")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( NULL, NULL, NULL, NULL, 'World')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1, NULL, NULL, NULL, 'Oceania')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1, NULL, NULL, 'Australia')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    1, NULL, 'New South Wales')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    1,    1, 'Sydney')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    1,    2, 'Blue Mountains')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    1,    3, 'Central Coast')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    1,    4, 'South Coast')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    2, NULL, 'Victoria')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    2,    1, 'Melbourne')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    1,    3, NULL, 'Queensland')")
+    try! db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES (    1,    2, NULL, NULL, 'New Zealand')")
+  }
+  
+  func testAddDuplicateLocation() {
+    XCTAssertNil(try? db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( NULL, NULL, NULL, NULL, 'World2')"))
+    XCTAssertNil(try? db().execute("INSERT INTO locations (l1, l2, l3, l4, name) VALUES ( 1, NULL, NULL, NULL, 'Oceania2')"))
+  }
+  
+  func testFindlevel0Location() {
+    XCTAssertEqual(Location.get(nil)?.name, "World")
   }
 
   func testFindLevel1Location() {
-    XCTAssertEqual(Location.get(1,nil,nil,nil)?.name, "Oceania")
+    XCTAssertEqual(Location.get(1)?.name, "Oceania")
   }
 
   func testFindLevel2Location() {
-    XCTAssertEqual(Location.get(1,2,nil,nil)?.name, "New Zealand")
+    XCTAssertEqual(Location.get(1,2)?.name, "New Zealand")
   }
 
   func testFindLevel3Location() {
-    XCTAssertEqual(Location.get(1,1,2,nil)?.name, "Victoria")
+    XCTAssertEqual(Location.get(1,1,2)?.name, "Victoria")
   }
 
   func testFindLevel4Location() {
@@ -44,20 +57,56 @@ class LocationTests: XCTestCase {
   }
 
   func testFindNonExistentLocation() {
-    XCTAssertNil(Location.get(99,nil,nil,nil))
+    XCTAssertNil(Location.get(99))
+  }
+  
+  func testFindParent() {
+    let level4 = Location.get(1,1,1,2)
+    XCTAssertNotNil(level4)
+    let level3 = level4?.getParent()
+    XCTAssertEqual(level3?.name, "New South Wales")
+    let level2 = level3?.getParent()
+    XCTAssertEqual(level2?.name, "Australia")
+    let level1 = level2?.getParent()
+    XCTAssertEqual(level1?.name, "Oceania")
+    let level0 = level1?.getParent()
+    XCTAssertEqual(level0?.name, "World")
+    XCTAssertNil(level0?.getParent())
+  }
+  
+  func testFindChildren() {
+    let level0 = Location.get(nil)
+    XCTAssertEqual(level0?.name, "World")
+    XCTAssertEqual(level0?.getChildren().count, 1)
+    let level1 = level0?.getChildren().first
+    XCTAssertEqual(level1?.name, "Oceania")
+    XCTAssertEqual(level1?.getChildren().count, 2)
+    let level2 = level1?.getChildren().first
+    XCTAssertEqual(level2?.name, "Australia")
+    XCTAssertEqual(level2?.getChildren().count, 3)
+    let level3 = level2?.getChildren().first
+    XCTAssertEqual(level3?.name, "New South Wales")
+    XCTAssertEqual(level3?.getChildren().count, 4)
+    let level4 = level3?.getChildren().first
+    XCTAssertEqual(level4?.name, "Sydney")
+    XCTAssertEqual(level4?.getChildren().count, 0)
   }
 
 }
 
+
 extension LocationTests {
   static var allTests : [(String, LocationTests -> () throws -> Void)] {
     return [
+      ("testAddDuplicateLocation", testAddDuplicateLocation),
       ("testFindLevel1Location", testFindLevel1Location),
       ("testFindLevel2Location", testFindLevel2Location),
       ("testFindLevel3Location", testFindLevel3Location),
       ("testFindLevel4Location", testFindLevel4Location),
       ("testFindInvalidLocation", testFindInvalidLocation),
       ("testFindNonExistentLocation", testFindNonExistentLocation),
+      ("testFindParent", testFindParent),
+      ("testFindChildren", testFindChildren),
     ]
   }
 }
