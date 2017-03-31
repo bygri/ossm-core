28c7be1d6b30b1e67b7a2ff8228cc8858ca5e66c
Toby Griffin <toby.griffin@gmail.com>
2017-03-31 14:28:56 +1100

    Add an implementation of Location, with tests

--- Sources/Geography/Location.swift
+++ Sources/Geography/Location.swift
@@ -4,15 +4,59 @@ import Localization
   A geographical location.
 
   Defined as a tree structure.
+
+  The map of Locations is held in memory.
 */
 public final class Location {
 
   public let name: TranslatableString
-  public let parent: Location?
+  public weak var parent: Location?
+  public var children: [Location] = []
 
   public init(name: String, parent: Location?) {
     self.name = name
-    self.parent = parent
+    if let parent = parent {
+      parent.add(child: self)
+    }
+  }
+
+  /*
+    An array of the parents of this Location, all the way back to the root.
+
+    The first element is this Location's immediate parent, the second is the
+    first element's parent, and so on.
+
+    Example response:
+
+      sydney.allParents // ["NSW", "Australia", "Oceania", "World"]
+  */
+  public var allParents: [Location] {
+    guard let parent = parent else { return [] }
+    return [parent] + parent.allParents
+  }
+
+  /*
+    An array of all child Locations which do not themselves have children.
+  */
+  public var allTerminalChildren: [Location] {
+    return children.reduce([]) {
+      $1.isTerminal ?
+        $0 + [$1] :
+        $0 + $1.allTerminalChildren
+    }
+  }
+
+  /*
+    True if the Location has no child Locations.
+  */
+  public var isTerminal: Bool {
+    return children.isEmpty
+  }
+
+  /// Add a child Location to this Location.
+  func add(child: Location) {
+    child.parent = self
+    children.append(child)
   }
 
 }
--- Tests/GeographyTests/GeographyTests.swift
+++ Tests/GeographyTests/GeographyTests.swift
@@ -5,6 +5,9 @@ class GeographyTests: XCTestCase {
 
   static var allTests = [
     ("testCreateLocation", testCreateLocation),
+    ("testCreateTree", testCreateTree),
+    ("testAllParents", testAllParents),
+    ("testTerminalChildren", testTerminalChildren),
   ]
 
   func testCreateLocation() {
@@ -18,6 +21,32 @@ class GeographyTests: XCTestCase {
     let node2 = Location(name: "NODE2", parent: root)
     XCTAssertEqual(node1.name, "NODE1")
     XCTAssertEqual(node2.parent, root)
+  }
+
+  func testAllParents() {
+    let root = Location(name: "ROOT", parent: nil)
+    let node1 = Location(name: "NODE1", parent: root)
+    let node2 = Location(name: "NODE2", parent: node1)
+    let node3 = Location(name: "NODE3", parent: node2)
+    XCTAssertEqual(node3.allParents, [node2, node1, root])
+  }
+
+  func testTerminalChildren() {
+    let root = Location(name: "ROOT", parent: nil)
+    let node1 = Location(name: "NODE1", parent: root) // terminal
+    let node2 = Location(name: "NODE2", parent: root)
+    let node21 = Location(name: "NODE2-1", parent: node2) // terminal
+    let node22 = Location(name: "NODE2-2", parent: node2)
+    let node221 = Location(name: "NODE2-2-1", parent: node22) // terminal
+    let node222 = Location(name: "NODE2-2-2", parent: node22) // terminal
+    let all = root.allTerminalChildren
+    XCTAssertFalse(all.contains(root))
+    XCTAssertTrue(all.contains(node1))
+    XCTAssertFalse(all.contains(node2))
+    XCTAssertTrue(all.contains(node21))
+    XCTAssertFalse(all.contains(node22))
+    XCTAssertTrue(all.contains(node221))
+    XCTAssertTrue(all.contains(node222))
   }
 
 }
