93e6fcb9afc1147aec83e094637097beeefcced6
Gligor Kotushevski <gligor@orbba.com>
2017-04-02 16:24:54 +1200

    adding initial timeline setup with a date and a timer that ticks every second

--- Sources/Calendar/Date.swift
+++ Sources/Calendar/Date.swift
@@ -1,1 +1,32 @@
-// todo
+
+public struct Date {
+  let year: Int
+  let day: Int
+  let hour: Int
+  let minute: Int
+  let second: Int
+
+  func addSecond() {
+    second += 1
+
+    if second >= 60 {
+      minute += 1
+      second = 0
+    }
+
+    if minute >= 60 {
+      hour += 1
+      minute = 0
+    }
+
+    if hour >= 24 {
+      day += 1
+      hour = 0
+    }
+
+    if day >= 365 {
+      year += 1
+      day = 0
+    }
+  }
+}
---
+++ Sources/Calendar/Timeline.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+/*
+  A timeline of the game simulation.
+
+  It uses a singleton instance of itself.
+
+  Keeps track of the game timeline in memory and should persist to database on every tick.
+*/
+public final class Timeline {
+
+  static let sharedInstance = Timeline()
+
+  // private init so Timeline is only used as a singleton instance
+  private init() {
+    gameDate = Date(year: 0, day: 0, hour: 0, minute: 0, second: 0)
+    timer = Timer(timeInterval: 1, target: gameDate, selector: #selector(Timeline.timerTick), userInfo: nil, repeats: true)
+  }
+
+  private var gameDate: Date
+  private var timer: Timer
+
+  /// tick the timer every second to add a second to the game time
+  dynamic func timerTick() {
+    gameDate.addSecond()
+
+    // TODO: add any other date time events here (ie. games every 7 days, training every day at certain hour etc.)
+  }
+}
