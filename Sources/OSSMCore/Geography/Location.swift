5ca8a2f6639bb8b3d22941599a7807f1aef9afa1
Toby Griffin <toby.griffin@gmail.com>
2017-04-08 18:17:43 +1000

    Made Location persistent in memory (#3)
    * Made Location persistent in memory
    
    * Update Location syntax to match Fluent entities

--- Sources/OSSMCore/Geography/Location.swift
+++ Sources/OSSMCore/Geography/Location.swift
@@ -1,24 +1,70 @@
-/*
+/**
   A geographical location.
 
   Defined as a tree structure.
 
-  The map of Locations is held in memory.
+  After building up your locations tree, call `Location.buildIndex(fromRoot:)`
+  with the root location to build the lookup index. This will allow you to use
+  `Location.fetch(at:)` to get locations by id number, along with standard
+  traversal properties such as `parent` and `children`.
 */
 public final class Location {
 
-  public let name: TranslatableString
-  public weak var parent: Location?
-  public var children: [Location] = []
-
-  public init(name: String, parent: Location?) {
-    self.name = name
-    if let parent = parent {
-      parent.add(child: self)
-    }
+  public enum Error: Swift.Error {
+    case duplicateLocationId(Int)
+    case noIndex
   }
 
-  /*
+  static var index: [Int: Location]? = nil
+
+  public let id: Int
+  public let name: TranslatableString
+  public weak var parent: Location?
+  public let children: [Location]
+
+  public init(id: Int, name: TranslatableString, children: [Location] = []) throws {
+    self.id = id
+    self.name = name
+    self.children = children
+    children.forEach { $0.parent = self }
+  }
+
+  /**
+    Return the Location with the given ID number, or `nil` if there is no
+    Location by that ID.
+
+    May throw `Location.Error.noIndex` if `Location.buildIndex(fromRoot:)` has
+    not yet been called.
+  */
+  static func find(_ id: Int) throws -> Location? {
+    guard let index = index else {
+      throw Error.noIndex
+    }
+    guard let loc = index[id] else {
+      return nil
+    }
+    return loc
+  }
+
+  /**
+    Build up the internal index of IDs to Locations. Call this after all
+    Locations have been instantiated. The root Location must be passed in.
+
+    May throw `Location.Error.duplicateLocationId` if a location ID has been
+    used more than once in the Location tree.
+  */
+  static func buildIndex(fromRoot root: Location) throws {
+    index = try ([root] + root.allChildren).reduce([:]) { acc, loc in
+      var index = acc
+      guard index[loc.id] == nil else {
+        throw Error.duplicateLocationId(loc.id)
+      }
+      index[loc.id] = loc
+      return index
+    } as [Int: Location]
+  }
+
+  /**
     An array of the parents of this Location, all the way back to the root.
 
     The first element is this Location's immediate parent, the second is the
@@ -33,7 +79,18 @@ public final class Location {
     return [parent] + parent.allParents
   }
 
-  /*
+  /**
+    An array of all child Locations in no particular order.
+  */
+  public var allChildren: [Location] {
+    return children.reduce([]) {
+      $1.isTerminal ?
+        $0 + [$1] :
+        $0 + [$1] + $1.allChildren
+    }
+  }
+
+  /**
     An array of all child Locations which do not themselves have children.
   */
   public var allTerminalChildren: [Location] {
@@ -44,17 +101,11 @@ public final class Location {
     }
   }
 
-  /*
+  /**
     True if the Location has no child Locations.
   */
   public var isTerminal: Bool {
     return children.isEmpty
-  }
-
-  /// Add a child Location to this Location.
-  func add(child: Location) {
-    child.parent = self
-    children.append(child)
   }
 
 }
@@ -66,9 +117,9 @@ extension Location: Equatable {
   }
 }
 
-// TODO: Remove or replace this once we have proper primary keys
+
 extension Location: Hashable {
   public var hashValue: Int {
-    return name.hashValue
+    return id
   }
 }
